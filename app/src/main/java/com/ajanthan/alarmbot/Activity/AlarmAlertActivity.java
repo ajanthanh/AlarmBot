ed2e3ffc523f69d243192d15714b1f2aa75ce085
Ajanthan Hariharan <ajanthanh@gmail.com>
2015-12-30 23:45:13 -0500

    finished mock alarm alert activity and layout

--- app/src/main/java/com/ajanthan/alarmbot/Activity/AlarmAlertActivity.java
+++ app/src/main/java/com/ajanthan/alarmbot/Activity/AlarmAlertActivity.java
@@ -3,17 +3,60 @@ package com.ajanthan.alarmbot.Activity;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.ajanthan.alarmbot.R;
+
+import java.util.Calendar;
 
 /**
  * Created by ajanthan on 15-12-20.
  */
 public class AlarmAlertActivity extends Activity {
 
+    private TextView tvTime;
+    private TextView tvAmPm;
+    private Button bSnooze;
+    private Button bDismiss;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alarm_alert);
+
+        tvTime = (TextView) findViewById(R.id.time);
+        tvAmPm = (TextView) findViewById(R.id.amPm);
+        bSnooze = (Button) findViewById(R.id.snooze);
+        bDismiss = (Button) findViewById(R.id.dismiss);
+
+        tvTime.setText(Calendar.getInstance().get(Calendar.HOUR) + ":" +
+                Calendar.getInstance().get(Calendar.MINUTE));
+        tvAmPm.setText((Calendar.getInstance().get(Calendar.HOUR) < 12) ? "AM" : "PM");
+
+        setDismissOnClickListener();
+        setSnoozeOnClickListener();
+
+    }
+    private void setSnoozeOnClickListener(){
+        bSnooze.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO implement a 5 minute delayed alarm
+                Log.e("AlarmAlert", "Snooze");
+            }
+        });
+    }
+
+    private void setDismissOnClickListener(){
+        bDismiss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO stop alarm
+                Log.e("AlarmAlert","Dismiss");
+            }
+        });
     }
 }
